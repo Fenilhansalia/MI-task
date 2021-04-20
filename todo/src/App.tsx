727ffdb417abf0d08cdf49ce2a4ab457dcff38d8
Fenilhansalia <hansaliafenil@gmail.com>
2021-04-20 13:09:30 +0530

    route setup

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
+export default function App(): JSX.Element {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/users'>Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/users'>
+            <Users />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home(): JSX.Element {
+  return <h2>Home</h2>;
+}
+
+function About(): JSX.Element {
+  return <h2>About</h2>;
+}
+
+function Users(): JSX.Element {
+  return <h2>Users</h2>;
+}
